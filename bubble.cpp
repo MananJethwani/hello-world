7803614524c2c6dec2154d5979978a7e98647a10
Ayush Sharma <56473411+itzaayush@users.noreply.github.com>
2020-10-03 14:47:58 +0530

    Update bubble.cpp
    Hey i This i have added comment and indentation

--- bubble.cpp
+++ bubble.cpp
@@ -1,21 +1,25 @@
+// Bubble Sort
 #include<iostream.h>
 using namespace std;
 int main()
 {
-int a[100],i,j,n,t;
-cin>>n;
-for(i=0;i<n;i++)
-{
-cin>>a[i];
-}for(i=0;i<n;i++)
-{
-for(j=0;j<n-1;j++)
-{if(a[j]>a[j+1])
-{t=a[j];
-a[j]=a[j+1];
-a[j+1]=t;
-}
-}
-}
-return 0;
+  int a[100],i,j,n,t; // variable deceleration
+  cin>>n;             // accepting value from user
+  for(i=0;i<n;i++)
+  {
+    cin>>a[i];
+  }
+  for(i=0;i<n;i++)   // loop that will sort the Array
+  {
+    for(j=0;j<n-1;j++)
+    {
+      if(a[j]>a[j+1])
+      {
+         t=a[j];
+         a[j]=a[j+1];
+         a[j+1]=t;
+      }
+    }
+  }
+  return 0;
 }
